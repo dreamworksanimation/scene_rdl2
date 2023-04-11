f483f668f8250d0953b09efc85a30b3faa060dff
kjeffery <keith.jeffery@dreamworks.com>
2023-04-11 16:35:03 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '11.7'
+    _version = '11.8'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
