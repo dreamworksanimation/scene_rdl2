d445d06e114561a84ec12cd968d3e18e4845fe7d
jlanz <Jon.Lanz@dreamworks.com>
2023-05-24 19:54:58 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '11.11'
+    _version = '11.12'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
