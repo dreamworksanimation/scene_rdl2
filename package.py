b8718655455fd2bc6619f7db8a9fb5e7174eb251
cegan <conrad.egan@dreamworks.com>
2023-06-27 14:50:57 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.2'
+    _version = '12.3'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
