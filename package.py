f8fabb7ad928a3e9c26c361a14afb615e4beee4a
alee1 <ashley.lee@dreamworks.com>
2023-07-25 15:39:33 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.6'
+    _version = '12.7'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
