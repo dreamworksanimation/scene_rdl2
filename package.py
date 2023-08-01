117617556777d20f0620b210f279699bfd76b93b
scottc <scott.cegielski@dreamworks.com>
2023-08-01 14:22:06 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.7'
+    _version = '12.8'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
