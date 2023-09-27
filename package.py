f312bfacbab256a18c0a35923b461aca9efb443b
mrday <mike.day@dreamworks.com>
2023-09-26 19:50:25 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.12'
+    _version = '12.13'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
