32ceb991182fd01341f57ea82a780dfc02565c38
Jon Lanz <Jon.Lanz@dreamworks.com>
2024-01-17 14:14:50 -0800

    Update license for 2024 (#870)

--- lib/scene/rdl2/rdlalib/constructors.lua
+++ lib/scene/rdl2/rdlalib/constructors.lua
@@ -1,4 +1,4 @@
--- Copyright 2023 DreamWorks Animation LLC
+-- Copyright 2023-2024 DreamWorks Animation LLC
 -- SPDX-License-Identifier: Apache-2.0
 
 -- Hook global identifier lookups by using the __index metamethod on the _G
--- lib/scene/rdl2/rdlalib/transforms.lua
+++ lib/scene/rdl2/rdlalib/transforms.lua
@@ -1,4 +1,4 @@
--- Copyright 2023 DreamWorks Animation LLC
+-- Copyright 2023-2024 DreamWorks Animation LLC
 -- SPDX-License-Identifier: Apache-2.0
 
 -- Convenience functions for computing translation, rotation, and scale
