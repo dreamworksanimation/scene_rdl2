915956329ea5b8923b36216a64d314c9c6aa1724
rpacker <randy.packer@dreamworks.com>
2024-04-09 21:20:04 -0700

    Bumped major version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '13.12'
+    _version = '14.0'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
