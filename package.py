96a756ba8c0cb483b04f9b466cbd13f62e4281fd
dmccann <dan.mccann@dreamworks.com>
2024-05-15 09:23:03 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.4'
+    _version = '14.5'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
