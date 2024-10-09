6766bd8a32e0a7af590ea7eb66583678fc397548
bart <build.and.release.tools@dreamworks.com>
2024-10-09 11:27:02 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.15'
+    _version = '14.16'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
