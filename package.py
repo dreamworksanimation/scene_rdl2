b661a0871bcce8258675cd82685af2bda4216ed9
206531218_nbcuni <toshi.kato@dreamworks.com>
2025-09-09 15:13:20 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '15.11'
+    _version = '15.12'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
